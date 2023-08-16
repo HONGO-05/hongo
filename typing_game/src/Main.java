84f9d907a45205f9d0b3241df127291da9c01c36
m-hongo <annabel3077@gmail.com>
2023-08-16 21:07:59 +0900

    テキストフィールドの値取得の調査用コミット

--- typing_game/src/Main.java
+++ typing_game/src/Main.java
@@ -1,10 +1,6 @@
 import front.window.MainWindow;
 import front.screen_mode.ScreenMode;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.File;
-
 public class Main {
     public static void main (String args[]) {
         MainWindow.getInstance(ScreenMode.TITLE);
--- typing_game/src/front/quiz/components/QuizGamePanel.java
+++ typing_game/src/front/quiz/components/QuizGamePanel.java
@@ -1,52 +1,108 @@
 package front.quiz.components;
 
+import front.quiz.QuizPanel;
+import front.screen_mode.ScreenMode;
+import front.window.MainWindow;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
-import java.util.List;
+
 
 public class QuizGamePanel extends JPanel{
     ImageIO img;
-    List<String> answer = new ArrayList<>();
-    List<String> question = new ArrayList<>();
+    JTextField answerText;
+    ArrayList<String> answer = new ArrayList<>();
+    ArrayList<String> question = new ArrayList<>();
+
     static int quizNum = 10;
-    static int resultCount = 0;
+    static int count = 0;
+
+
 
     public QuizGamePanel() {
         this.setLayout(null);
+    }
 
+    public void prepareComponents(){
+//        MyKeyListener myKeyListener = new MyKeyListener(this);
+        answerText = new JTextField();
+        answerText.setBounds(160, 300, 400, 70);
+        answerText.setFont(new Font("ヒラギノ角ゴ ProN", Font.BOLD, 20));
+
+        JButton btn_clear = new JButton("clear");
+        btn_clear.setBounds(350, 240,200, 50);
+        btn_clear.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                answerText.getText();
+                answerText.setText("");
+            }
+        });
+
+        ImageIcon icon = new ImageIcon(System.getProperty("user.dir") + "/src/image/game_background.gif");
+        JLabel backGroundLabel = new JLabel(icon);
+        backGroundLabel.setBounds(0, 0, 720, 540);
+
+        this.add(answerText);
+        this.add(btn_clear);
+        this.add(backGroundLabel);
     }
 
     public ArrayList<String> questionList(){
         ArrayList<String> questions = new ArrayList<>();
         questions.add("サモエド");
+        questions.add("ボルゾイ");
+        questions.add("柴犬");
+        questions.add("ジャックラッセルテリア");
+        questions.add("ポメラニアン");
+        questions.add("ミニチュア・シュナウザー");
+        questions.add("ウェルシュ・コーギー・ペンブローク");
+        questions.add("パグ");
+        questions.add("ビション・フリーゼ");
+        questions.add("バセット・ハウンド");
         return questions;
     }
 
-    public int judge(ArrayList<String> answers){
-        ArrayList<String> question = this.questionList();
-        for (int i = 0; i < quizNum; i++){
-            if (question.get(i).equals(answers.get(i))) {
-                resultCount++;
-            }
-        }
-        return resultCount;
-    }
 
-    public void prepareComponents(){
-        JTextField answerText = new JTextField(30);
-        answerText.setBounds(160, 300, 400, 70);
-        answerText.setFont(new Font("ヒラギノ角ゴ ProN", Font.BOLD, 20));
-        this.add(answerText);
+//    public int judge(ArrayList<String> answers){
+//        ArrayList<String> question = this.questionList();
+//        for (int i = 0; i < quizNum; i++){
+//            if (question.get(i).equals(answers.get(i))) {
+//               count++;
+//            }
+//        }
+//        return count;
+//    }
 
-        answer.add(answerText.getText());
-
-        ImageIcon icon = new ImageIcon(System.getProperty("user.dir") + "/src/image/game_background.gif");
-        JLabel backGroundLabel = new JLabel(icon);
-        backGroundLabel.setBounds(0, 0, 720, 540);
-        this.add(backGroundLabel);
-    }
-
+//    private class MyKeyListener implements KeyListener{
+//        QuizGamePanel quizGamePanel;
+//        MyKeyListener(QuizGamePanel q){
+//            super();
+//            quizGamePanel = q;
+//            quizGamePanel.addKeyListener(this);
+//        }
+//        @Override
+//        public void keyTyped(KeyEvent e) {
+//            switch (e.getKeyCode()){
+//                case KeyEvent.VK_ENTER:
+//                    answer.add(answerText.getText());
+//                    answerText.setText("テスト");
+//                    count++;
+//                    break;
+//            }
+//        }
+//
+//        @Override
+//        public void keyPressed(KeyEvent e) {
+//        }
+//        @Override
+//        public void keyReleased(KeyEvent e) {
+//        }
+//    }
 
 }
